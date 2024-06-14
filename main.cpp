e8e225aebbc67263524e2e81ce4ef4ccee85b165
Eric-Mota <198753@upf.br>
2024-06-14 13:45:20 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <locale>
 #include <codecvt>
 #include <cwctype>
+#include <queue>
 
 struct Node{
     int frequency;
@@ -11,21 +12,17 @@ struct Node{
     Node *right, *left;
     
 
-    // Inicializa o nodo com a frequencia e a palavra fornecida 
+    // Construtor que inicializa o Node com a frequência e a palavra fornecidas
     Node(int freq, wchar_t w) : frequency(freq), word(w), right(nullptr), left(nullptr) {}
 
     Node(int freq, wchar_t w, Node *l, Node *r) : frequency(freq), word(w), right(l), left(r) {}
     
 };
 
-struct Lista{
-    Node* begin;
-    int size;
-
-    Lista() : begin(nullptr), size(0) {}
-
-    Lista(Node* inicio) : begin(inicio), size(1) {}
-
+struct CompareNodes {
+    bool operator()(const Node* lhs, const Node* rhs) const {
+        return lhs->frequency > rhs->frequency;
+    }
 };
 
 //cria a tabela de frequencia
@@ -37,6 +34,15 @@ std::unordered_map<wchar_t, int> createFrequencyMap (std::wifstream &file) {
         frequencyMap[c]++;
     }
     return frequencyMap;
+}
+
+//Fila para a criação da arvore
+std::priority_queue<Node*, std::vector<Node*>, CompareNodes> createPriorityQueue (std::unordered_map<wchar_t, int> frequencyMap) {
+    std::priority_queue<Node*, std::vector<Node*>, CompareNodes> priorityQueue;
+    for (auto c : frequencyMap) {
+        priorityQueue.push(new Node(c.second, c.first));
+    }
+    return priorityQueue;
 }
 
 //Testes
@@ -52,6 +58,16 @@ void printFrequencyMap (std::unordered_map<wchar_t, int> &frequencyMap) {
     }
 }
 
+void printPriorityQueue (std::priority_queue<Node*, std::vector<Node*>, CompareNodes> priorityQueue) {
+
+    while (!priorityQueue.empty() ) {          
+        Node aux = *priorityQueue.top();
+        std::wcout << aux.word << " " << aux.frequency << "\n";
+        priorityQueue.pop();
+    }
+
+}
+
 int main() {
 
     std::string nomeArquivo = "../teste.txt";
@@ -61,16 +77,22 @@ int main() {
     file.imbue(std::locale(file.getloc(), new std::codecvt_utf8<wchar_t>));
 
     // Verifica se o arquivo foi aberto corretamente
-    !file.is_open() ? (std::wcerr << L"Nao foi possivel abrir o arquivo." << std::endl, 1) : 0;
+    if (!file.is_open()) {
+        std::wcerr << L"Nao foi possivel abrir o arquivo." << std::endl;
+        return 1;
+    }
 
     std::unordered_map<wchar_t, int> frequencyMap;
+    std::priority_queue<Node*, std::vector<Node*>, CompareNodes> filaOrdenada;
     
     //cria a tabela de frequncia
     frequencyMap = createFrequencyMap(file);
     file.close();
 
+    filaOrdenada = createPriorityQueue(frequencyMap);
     
-    printFrequencyMap(frequencyMap);
+    //printFrequencyMap(frequencyMap);
+    //printPriorityQueue(filaOrdenada);
 
     return 0;
 }
